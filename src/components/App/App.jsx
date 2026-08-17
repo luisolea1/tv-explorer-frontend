b83faadce8f3841f9cd0e43a3091bcc04c4944d4
Luis Alberto Olea Olea <luisolea653@gmail.com>
2026-08-17 14:12:38 -0600

    Reorganiza la estructura inicial de la aplicación

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <main className="app__content">
+        <h1 className="app__title">TV Explorer</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
